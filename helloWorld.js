7fad576a6a1f7b211de64b12694dbfbdeb68dec4
Oleksandr Andronyak <sacha.1994@icloud.com>
2023-03-08 13:24:32 +0000

    Cloned Repository

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,13 @@
 // //! Program to display hello world
 // alert('Hello World!');
 
-//! New message 
-alert('Git is Awesome!');
+// //! New message 
+// alert('Git is Awesome!');
+
+//! user's inputs
+let name = prompt('Enter your name');
+let num = prompt('Enter your favorite number');
+
+//! Output for the user
+alert(`Welcome ${name}!`);
+alert(`${num}?! That's my favorite number too!`);
